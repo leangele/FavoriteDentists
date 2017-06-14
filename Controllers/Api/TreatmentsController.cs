1ce2d953de0faf800d80c230056c9dbe6b9a754a
leangele <angelocardona85@gmail.com>
2017-06-13 23:07:56 -0400

    creation of treatment controller

---
+++ Controllers/Api/TreatmentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FavoriteDentists.Models;
+
+namespace FavoriteDentists.Controllers.Api
+{
+    public class TreatmentsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Treatments
+        public IQueryable<Treatment> GetTreatments()
+        {
+            return db.Treatments;
+        }
+
+        // GET: api/Treatments/5
+        [ResponseType(typeof(Treatment))]
+        public async Task<IHttpActionResult> GetTreatment(int id)
+        {
+            Treatment treatment = await db.Treatments.FindAsync(id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(treatment);
+        }
+
+        // PUT: api/Treatments/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutTreatment(int id, Treatment treatment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != treatment.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(treatment).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TreatmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Treatments
+        [ResponseType(typeof(Treatment))]
+        public async Task<IHttpActionResult> PostTreatment(Treatment treatment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Treatments.Add(treatment);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = treatment.Id }, treatment);
+        }
+
+        // DELETE: api/Treatments/5
+        [ResponseType(typeof(Treatment))]
+        public async Task<IHttpActionResult> DeleteTreatment(int id)
+        {
+            Treatment treatment = await db.Treatments.FindAsync(id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            db.Treatments.Remove(treatment);
+            await db.SaveChangesAsync();
+
+            return Ok(treatment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TreatmentExists(int id)
+        {
+            return db.Treatments.Count(e => e.Id == id) > 0;
+        }
+    }
+}
